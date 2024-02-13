d3a5af49b2bdaed343e3f92465fd4e056becae39
gerarmc <gerardomelendezpro@gmail.com>
2024-02-13 18:41:52 -0400

    challenge number added14.1

--- javascript/functionsAndExecutionContext.js
+++ javascript/functionsAndExecutionContext.js
@@ -308,4 +308,15 @@ function addingAllTheWeirdStuff(array1, array2){
  console.log(addingAllTheWeirdStuff([1, 3, 5, 17, 15], [1, 2, 3, 4, 5])); // expected log [10, 12, 14, 23, 21]
  console.log(addingAllTheWeirdStuff([1, 3, 5, 17, 15, 1], [1, 2, 3, 4, 5, 22])); // expected log [11, 13, 15, 46, 44, 11]
 
+ //14. Write a function disemvowel that takes in a string and returns a new string with all vowels removed.
+
+ function disemvowel(string) {
+  // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+// console.log(disemvowel('CodeSmith')); // => 'CdSmth'
+// console.log(disemvowel('BANANA')); // => 'BNN'
+// console.log(disemvowel('hello world')); // => 'hll wrld'
+
 
