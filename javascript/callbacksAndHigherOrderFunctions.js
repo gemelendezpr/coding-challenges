5b535ddded14719d2fbbb5f3ce327fa2f21e7409
gerarmc <gerardomelendezpro@gmail.com>
2024-02-24 23:36:46 -0400

    challenge callbacks  added2

--- javascript/callbacksAndHigherOrderFunctions.js
+++ javascript/callbacksAndHigherOrderFunctions.js
@@ -20,3 +20,40 @@ function pluralize(array) {
   // Uncomment these to check your work!
   const animals = ["dog", "cat", "tree frog"];
   console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+
+  //2. Create a function subtractTwo that accepts a number and returns that number minus 2.
+
+// Then create a function map that takes two inputs -
+
+// an array of numbers (a list of numbers)
+// a 'callback' function - this function is applied to each element of the array (inside of the function 'map')
+// Have your map function return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array. Please do not use the native map or forEach method.
+
+// map([3,4,5], subtractTwo); //-> [1,2,3]
+// subtractTwo(10); //-> 8
+// subtractTwo(12); //-> 10
+
+//  Solution:
+
+// ADD CODE HERE
+function subtractTwo(number) {
+    return number - 2;
+  }
+  
+  function map(array, callback) {
+    // Initialize an empty array to store the result
+    const mappedArray = [];
+  
+    // Iterate through each element in the input array
+    for (let i = 0; i < array.length; i++) {
+      // Apply the 'callback' function to each element and push the result to the result array
+      mappedArray.push(callback(array[i]));
+    }
+  
+    // Return the result array
+    return mappedArray;
+  }
+  // Uncomment these to check your work!
+  console.log(typeof subtractTwo); // should log: 'function'
+  console.log(typeof map); // should log: 'function'
+  console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
