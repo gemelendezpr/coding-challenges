316cd8cbfbdecadb3e5aa4e98b8aab5a61d07a17
gerarmc <gerardomelendezpro@gmail.com>
2024-02-25 20:09:58 -0400

    challenge callbacks  added3

--- javascript/callbacksAndHigherOrderFunctions.js
+++ javascript/callbacksAndHigherOrderFunctions.js
@@ -57,3 +57,49 @@ function subtractTwo(number) {
   console.log(typeof subtractTwo); // should log: 'function'
   console.log(typeof map); // should log: 'function'
   console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
+
+//   Challenge: forEach
+
+// Part 1
+// Create a function forEach which takes an array and a callback, and runs the callback on each element of the array. forEach does not return anything. Please do not use the native forEach or map method.
+
+// let alphabet = '';
+// const letters = ['a', 'b', 'c', 'd'];
+// forEach(letters, function(char) {
+//   alphabet += char;
+// });
+// console.log(alphabet); //prints 'abcd'
+// Part 2
+// Now let's rebuild map from the previous challenge. This time instead of using a for loop, you're going to use the forEach we just created.
+
+// Solution
+
+// ADD CODE HERE
+// Part 1
+function forEach(array, callback) {
+    // Run the callback on each element of the array
+    for (let i = 0; i < array.length; i++) {
+      callback(array[i]);
+    }
+  }
+  
+  // Part 2
+  function map(array, callback) {
+    // Initialize an empty array to store the result
+    const mappedArray = [];
+  
+    // Use the forEach function to apply the callback on each element
+    forEach(array, function(element) {
+      // Push the result to the result array
+      mappedArray.push(callback(element));
+    });
+  
+    // Return the result array
+    return mappedArray;
+  }
+  
+  // Uncomment these to check your work!
+  console.log(typeof forEach); // should log: 'function'
+  forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+  console.log(typeof map); // should log: 'function'
+  console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
