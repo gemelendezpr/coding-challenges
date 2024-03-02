ece45e803753a67bfa5c8e1ab1c34ce72170ee06
gerarmc <gerardomelendezpro@gmail.com>
2024-03-02 02:14:27 -0400

    challenge callbacks  added6.1

--- javascript/callbacksAndHigherOrderFunctions.js
+++ javascript/callbacksAndHigherOrderFunctions.js
@@ -169,8 +169,6 @@ function filterArray(array, callback) {
   }
   return newArray;
 }
-const arrOfNums = [10, 35, 105, 9];
-const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
-const over100 = n => n > 100;
+
 const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
 console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
