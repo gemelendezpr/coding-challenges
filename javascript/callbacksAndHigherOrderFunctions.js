7c793525595497561bc53cd78292519fb675d82f
gerarmc <gerardomelendezpro@gmail.com>
2024-03-06 18:52:58 -0400

    challenge callbacks  added10

--- javascript/callbacksAndHigherOrderFunctions.js
+++ javascript/callbacksAndHigherOrderFunctions.js
@@ -251,3 +251,30 @@ const arr1 = [5, 10, 15];
 const arr2 = [15, 88, 1, 5, 7];
 const arr3 = [100, 15, 10, 1, 5];
 console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
+
+//10. Challenge: objOfMatches
+
+// Construct a function objOfMatches that accepts two arrays and a callback. objOfMatches will build an object and return it. To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. 
+//If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
+
+// ADD CODE HERE
+function objOfMatches(arr1, arr2, callback) {
+
+  const resultObj = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+    const key = arr1[i];
+    const value = arr2[i];
+
+    if (callback(key) === value) {
+      resultObj[key] = value;
+    }
+  }
+
+  return resultObj;
+}
+// Uncomment these to check your work!
+const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
+const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
+function uppercaser(str) { return str.toUpperCase(); }
+console.log(objOfMatches(arr1, arr2, uppercaser)); // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
