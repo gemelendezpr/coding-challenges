b99b570d48ef1f58a7c7198153cf6da91e561b1a
gerarmc <gerardomelendezpro@gmail.com>
2024-03-19 16:21:36 -0400

    closure, scope & execution context exercise solution2

--- javascript/closureScopeAndExecutionContext.js
+++ javascript/closureScopeAndExecutionContext.js
@@ -20,3 +20,32 @@ function createFunction() {
   // Uncomment these to check your work!
   const myFunction = createFunction();
   console.log(myFunction()); //should log: 'hello world'
+
+  //2. Challenge: createFunctionWithInput
+
+// Create a function createFunctionWithInput that accepts one input and returns a function. When that created function is called, it should return the input that was passed to createFunctionWithInput when it was created.
+
+// const sampleFunc = createFunctionWithInput('sample');
+// const helloFunc = createFunctionWithInput('hello');
+
+// // Now we'll call the functions we created and log the result
+// console.log(sampleFunc()); 
+// // should log 'sample' to the console
+// console.log(helloFunc());
+// // should log 'hello' to the console
+
+// Solution
+// ADD CODE HERE
+// Define a function named createFunctionWithInput that accepts one parameter, input.
+function createFunctionWithInput(input) {
+    // Inside the function, return another function that takes no parameters.
+    return function() {
+      // This inner function returns the value of the input parameter passed to createFunctionWithInput.
+      return input;
+    };
+  }
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+  const sampleFunc = createFunctionWithInput('sample');
+  console.log(sampleFunc()); // should log: 'sample'
+  const helloFunc = createFunctionWithInput('hello');
+  console.log(helloFunc()); // should log: 'hello'
