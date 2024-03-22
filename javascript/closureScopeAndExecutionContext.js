61b792945a12f2f42a28070fb3d2b8374bc34714
gerarmc <gerardomelendezpro@gmail.com>
2024-03-21 22:22:58 -0400

    closure, scope & execution context exercise solution4

--- javascript/closureScopeAndExecutionContext.js
+++ javascript/closureScopeAndExecutionContext.js
@@ -89,4 +89,42 @@ function outer() {
   
   // Calls the function assigned to willCounter again. It continues incrementing the counter from where it left off, so the counter value will be 4.
   willCounter(); // counter 4
+
+  //4. Challenge: addByX
+
+// Now we are going to create a function addByX that returns a function that will add an input by x.
+
+// const addByTwo = addByX(2);
+// addByTwo(1); //should return 3
+// addByTwo(2); //should return 4
+// addByTwo(3); //should return 5
+
+// const addByThree = addByX(3);
+// addByThree(1); //should return 4
+// addByThree(2); //should return 5
+
+// const addByFour = addByX(4);
+// addByFour(4); //should return 8
+// addByFour(10); //should return 14
+
+// ADD CODE HERE
+// Defines a function named addByX that takes one parameter x.
+function addByX(x) {
+    // Returns an inner function that takes one parameter num and returns the sum of x and num.
+    return function(num) {
+      return x + num;
+    };
+  }
+  
+  // Assigns the result of calling addByX with argument 2 to the variable addByTwo.
+  const addByTwo = addByX(2);
+  
+  // Now call addByTwo with an input of 1 and log the output
+  // Calls addByTwo with an input of 1 and logs the output, which should be 2 + 1 = 3.
+  console.log(addByTwo(1)); // should return 3
+  
+  // Now call addByTwo with an input of 2 and log the output
+  // Calls addByTwo with an input of 1 and logs the output, which should be 2 + 1 = 3.
+  console.log(addByTwo(1)); // should return 3
+  
   
