9beedfb36b41a36b7f9607e99430d9f0fce585d6
gerarmc <gerardomelendezpro@gmail.com>
2024-03-26 15:57:33 -0400

    codesignal arcade practice2

--- codesignal/arcade.js
+++ codesignal/arcade.js
@@ -1,4 +1,5 @@
-//1. Given the string, check if it is a palindrome.
+//1. Challenge: checkPalindrome
+//Given the string, check if it is a palindrome.
 
 // Example
 
@@ -19,7 +20,31 @@ function solution(inputString) {
     let reversedWord = inputString.split('').reverse().join('')
     return reversedWord === inputString
     }
-
     
 console.log(solution('racecar'), 'true')
 console.log(solution('spongebob'), 'flase')
+
+
+//2. Challenge: centuryFromYear
+
+// Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+// Example
+
+// For year = 1905, the output should be
+// solution(year) = 20;
+// For year = 1700, the output should be
+// solution(year) = 17.
+
+function solution(year) {
+    // calculate the century by divinding the year by 100 and rounding up to the nearest integer.
+    let century = Math.ceil(year/100); 
+    // Return the calculated century
+    return century;
+}
+
+// Example usage:
+// Call the solution function with year = 1905 and log the result.
+console.log(solution(1905)); // Output: 20
+// Call the solution function with year = 1700 and log the result.
+console.log(solution(1700)); // Output: 17
