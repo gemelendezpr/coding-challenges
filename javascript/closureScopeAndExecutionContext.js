05ee6f80ca7328f6cc40529433baca64dd7fa6f2
gerarmc <gerardomelendezpro@gmail.com>
2024-03-29 22:49:57 -0400

    closure, scope & execution context challenge 7

--- javascript/closureScopeAndExecutionContext.js
+++ javascript/closureScopeAndExecutionContext.js
@@ -199,3 +199,28 @@ function after(times, callback) {
   console.log(afterCalled('world')); // -> undefined is printed
   console.log(afterCalled('world')); // -> undefined is printed
   console.log(afterCalled('world')); // -> 'hello world' is printed
+
+  //7. Challenge: delay
+
+// Write a function delay that accepts two arguments, a callback and the wait time in milliseconds. Delay should return a function that, when invoked waits for the specified amount of time before executing. HINT - research setTimeout();
+
+// Is it working? Check my answer
+// delay should create and return a function
+
+// The function returned from delay should only execute the callback after the specified wait time
+
+  // ADD CODE HERE
+function delay(callback, waitTime) {
+    // Define a function that will be returned by delay
+    return function() {
+      // Inside the returned function, use setTimeout to delay the execution of the callback
+      setTimeout(callback, waitTime);
+    };
+  }
+  
+  // UNCOMMENT THE CODE BELOW TO TEST DELAY
+  let count = 0;
+  const delayedFunc = delay(() => count++, 1000);
+  delayedFunc();
+  console.log(count); 												 // should print '0'
+  setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
