3ccfcbb994db673bfc477847eae103b6920eba1e
gerarmc <gerardomelendezpro@gmail.com>
2024-06-07 00:10:23 -0400

    javascript beginners assessment  evaluated

--- javascript/javascriptBeginners.js
+++ javascript/javascriptBeginners.js
@@ -62,3 +62,125 @@ If you are interested in attending CS Prep after this course and have not alread
 
 Create a function applyAll that takes an array and a callback as arguments. applyAll will return a new array where each element from the original array will have had the callback applied to it.
 */
+
+// Challenge 1:
+// You have a list of songs you want to play in an array “songs”. Define a function musicPlayer that takes one argument, an array of objects, where each object is a song. musicPlayer will iterate/loop through the array and print all the songs and artists to the console in the following format: 'Now playing: by '.
+
+function musicPlayer(songs) {
+
+  for (let i = 0; i < songs.length; i++) {
+    let song = songs[i];
+
+    console.log(`Now playing: ${song.songName} by ${song.artistName}`);
+  }
+};
+
+
+// UNCOMMENT THE CODE BELOW TO CHECK YOUR WORK
+// You'll know you're right if the console output logs all of the songs listed below, in order. 
+
+const songs = [
+  {songName: "Toxic", artistName: "Britney Spears"},
+  {songName: "Sun is Sunshine", artistName: "Bob Marley"},
+  {songName: "Easy On Me", artistName: "Adele"},
+  {songName: "DNA", artistName: "BTS"},
+];
+musicPlayer(songs); // => should log:
+  // Now playing: "Toxic" by Britney Spears
+  // Now playing: "Sun is Sunshine" by Bob Marley
+  // Now playing: "Easy On Me" by Adele
+  // Now playing: "DNA" by BTS
+
+
+
+// CHALLENGE 2 - ADD CODE BELOW
+// Create a function called findOddNumbers that takes one argument called “numArray", which is an array of numbers. When findOddNumbers is invoked it will return a new array with all of the numbers that are odd.
+
+oddNumsArray = [];
+
+function findOddNumbers(numArray) {
+  // iterate through the array
+  // if the num is not divisible by 2 push it to the new array
+  // return the new array
+  let oddNumsArray = [];
+  for (let i = 0; i <numArray.length; i++) {
+    if (numArray[i] % 2 === 1) {
+      oddNumsArray.push(numArray[i])
+    }
+  }
+  return oddNumsArray;
+}
+
+
+
+
+// UNCOMMENT THESE LINES TO CHECK YOUR WORK
+console.log(findOddNumbers([90013, 11801, 10012, 10006, 67001])); // => should return [90013, 11801, 67001]
+
+
+
+// CHALLENGE 3 - ADD CODE BELOW
+// Our design team wants to get more attraction to our page and decided to experiment with the title of our page. We want to capitalized the first three character of each title.
+
+
+
+// create a function called capitalizeThreeLetters which takes a word(string) as an argument. 
+// initialize a variable to an empty string to receive the capitalized letters
+// initialzie a variable for the rest of the string not capitalized
+// iterate through the string
+// use a method to capitalize the index letter (.toUpperCase())
+// used a method to select the start index of the string and where to end (substring)
+// when the function in invoked it will return the string with the first 3 letters capitalized 
+
+
+function capitalizeThreeLetters(str) {
+  let firstThreeLetters = str.substring(0, 3).toUpperCase();
+  let remainingStr = str.substring(3);
+
+  return firstThreeLetters + remainingStr;
+
+};
+
+
+
+
+// // UNCOMMENT THESE LINES TO CHECK YOUR WORK
+// console.log(capitalizeThreeLetters('welcome')); // => should return 'WELcome'
+// console.log(capitalizeThreeLetters('codesmith')); // => should return 'CODesmith'
+
+
+// // CS PREP ADMISSIONS CHALLENGE - ADD CODE BELOW
+// // Create a function applyAll that takes an array and a callback as arguments. applyAll will return a new array where each element from the original array will have had the callback applied to it.
+
+
+function applyAll(array, callback) {
+
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+}
+
+
+// // UNCOMMENT THESE LINES TO CHECK YOUR WORK
+console.log(applyAll(['mai', 'brian', 'jeho', 'han'], capitalizeThreeLetters)); // => should return ['MAI', 'BRIan', 'JEHo', 'HAN']
+
+/*
+
+You must pass 3/3 challenges to pass the assessment.
+
+Challenge 1 - Pass
+
+Challenge 2 - Pass
+
+Challenge 3 - Pass
+
+Part 2: Pass
+
+--
+
+Congrats - assessment passed as well as Part 2!! You did a great job. 
+
+*/
