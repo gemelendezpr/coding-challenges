81519684fa687a3fd48d9e4b652ef1e666722d8b
gerarmc <gerardomelendezpro@gmail.com>
2024-06-16 15:26:43 -0400

    csBin BONUS CHALLENGES ANSWERS PROVIDED BY INSTRUCTOR jun 15 added

--- javascript/csBin.js
+++ javascript/csBin.js
@@ -737,6 +737,342 @@ const NewNumArray = new NumArray();
 // countMost();
 // should print '1, 2, 4, 5'
 
+// <<< ---------------------------------------------------------------------- CS Prep Cohort 122 (2024) CSBIN Saturday Jun 15 Challenges ---------------------------------------------------------------------- >>>
+
+/*
+Big Data
+Using Higher Order Functions to Reduce Large Amounts of Data
+Let's use higher order functions like map, filter, and reduce to gather information and manipulate some data. Here we are importing historical Bitcoin data from 2013 to 2015. Each object contains information like price, market cap, fees, and much more. Let’s see what information we can extract using higher order functions!
+
+To begin scroll down to line 8034 - click and drag the scrollbar that appears on the right as you begin scrolling to get there quickly.
+
+First, let's log the bitcoinData object to investigate the structure of our data.
+What is the data type of bitcoinData? Let’s say we just want to look at bitcoin information on May 1st, 2013. How can we do that?
+Let’s say we want to create an array containing only the date and price of each day. Use the built-in map method to create an array containing that information.
+Let’s create an array that only includes days when exchange volume was not 0. Which method should we use?
+Everyday, new Bitcoins are generated. If we want to find out how many were generated all together during the time that’s provided in the dataset, can you figure out how to calculate it using reduce?
+Bitcoin price fluctuates a lot. Let’s combine some array methods to find the total number of days when bitcoin price was over $100!
+And finally, let’s find the average bitcoin transaction fees between 2013 and 2015. Be sure to floor this value.
+## Bonus Challenges
+Move onto these challenges only if you have finished the main challenges
+
+1. dayMapper (follow up to challenge 3)
+
+- Write a function called dayMapper. It will accept three parameters: arr (bitcoinData), property1, property2.
+- It should use map to return a new array of objects with only the passed in properties on each object.
+- If the passed in properties are not present in arr's objects, then it should ignore that property name and it should not appear in the returned array.
+2. averageValueOf________ (follow up to challenge 7)
+
+- for each property that has numeric values in bitcoin data, write a function that takes in an array (bitcoinData in this case) and a property name and will return the property's average value in the array.
+3. averageValueBonus (double follow up to challenge 7)
+
+- write a function called generalAverageValue that takes two parameters: arr and property.
+- arr will be bitcoinData and property will be a property name from the array of objets.
+- your function should check to make sure that property's values are numeric. if they're not, it should return an error message of your choosing. If you'd like, you can research throw new Error() and do that instead
+- your function should return an array of the form [property, avgValue]
+4. Tally bitcoin prices by range
+
+- write a function called priceRangeTally which accepts one argument, arr. Your function should do the following:
+- find the maximum bitcoin price
+- find the minimum bitcoin price
+- choose an arbitrary interval to create ranges (you can do this dynamically or hard code it, up to you)
+- Tally how many days of bitcoin prices fall into each range
+- console.log a nicely formatted string of each interval and how many days fall into each interval.
+- you don't have to return anything, but you can return a tally object or array of your choosing.
+5. write a function that will accept an array (bitcoin data) as a param, and return an object with keys that are the names of all the numerical properties of the bitcoindata objects, and whose values are the max values of those properties
+
+5.5. do the same thing, but for minimum values
+
+6. write a function that compares the two objects from number 5 and creates a new object with the same keys, but the values are the ranges of Max - Min
+
+EXAMPLE OF THE DATA
+const bitcoinData = [
+  {
+    "date": "2013-05-01",
+    "txVolume(USD)": 108659660.293,
+    "txCount": 52443,
+    "marketcap(USD)": 1542820000,
+    "price(USD)": 139,
+    "exchangeVolume(USD)": 0,
+    "generatedCoins": 3575,
+    "fees": 36.80599998,
+    "activeAddresses": null
+  },
+  {
+    "date": "2013-05-02",
+    "txVolume(USD)": 96958519.0041,
+    "txCount": 55169,
+    "marketcap(USD)": 1292190000,
+    "price(USD)": 116.38,
+    "exchangeVolume(USD)": 0,
+    "generatedCoins": 3425,
+    "fees": 54.40791613,
+    "activeAddresses": null
+  },
+  {
+    "date": "2013-05-03",
+    "txVolume(USD)": 84459697.3245,
+    "txCount": 55636,
+    "marketcap(USD)": 1180070000,
+    "price(USD)": 106.25,
+    "exchangeVolume(USD)": 0,
+    "generatedCoins": 3650,
+    "fees": 48.52677208,
+    "activeAddresses": null
+  },
+  {
+    "date": "2013-05-04",
+    "txVolume(USD)": 41545844.5296,
+    "txCount": 48595,
+    "marketcap(USD)": 1089890000,
+    "price(USD)": 98.1,
+    "exchangeVolume(USD)": 0,
+    "generatedCoins": 3900,
+    "fees": 43.41969441,
+    "activeAddresses": null
+  },
+  {
+    "date": "2013-05-05",
+    "txVolume(USD)": 56205929.5508,
+    "txCount": 49907,
+    "marketcap(USD)": 1254760000,
+    "price(USD)": 112.9,
+    "exchangeVolume(USD)": 0,
+    "generatedCoins": 3875,
+    "fees": 38.40895622,
+    "activeAddresses": null
+  },
+};
+
+*/
+
+
+
+
+
+/*
+//1. First, let's log the bitcoinData object to investigate the structure of our data.
+// console.log(bitcoinData);
+
+//2. What is the data type of bitcoinData? Let’s say we just want to look at bitcoin information on May 1st, 2013. How can we do that?
+//const dateFiltered = bitcoinData.filter(el => el.date === "2013-05-01");
+// console.log("dateFiltered - ", dateFiltered);
+
+//3. Let’s say we want to create an array containing only the date and price of each day. Use the built-in map method to create an array containing that information.
+//const dateAndPrice = bitcoinData.map(el => {
+//  return  {date: el.date, price: el["price(USD)"]}; 
+//}); 
+//console.log(dateAndPrice);
+
+//4. Let’s create an array that only includes days when exchange volume was not 0. Which method should we use?
+const volumeFiltered = bitcoinData.filter(el => el["exchangeVolume(USD)"] !== 0);
+//console.log("volumeFiltered - ", volumeFiltered);
+
+const zeroVolumeFiltered = bitcoinData.filter(el => el["exchangeVolume(USD)"] === 0);
+//console.log("zeroVolumeFiltered - ", zeroVolumeFiltered);
+
+console.log(volumeFiltered.length, zeroVolumeFiltered.length);
+
+//5. Everyday, new Bitcoins are generated. If we want to find out how many were generated all together during the time that’s provided in the dataset, can you figure out how to calculate it using reduce?
+const totalGeneratedCoins = bitcoinData.reduce((total, el) => {
+  return total + el.generatedCoins;
+}, 0);
+
+let generatedSum = 0;
+for(let i = 0; i < bitcoinData.length; i++) {
+  generatedSum += bitcoinData[i].generatedCoins;
+}
+
+console.log("Reduced - ", totalGeneratedCoins);
+console.log("For Loop - ", generatedSum);
+
+//6. Bitcoin price fluctuates a lot. Let’s combine some array methods to find the total number of days when bitcoin price was over $100!
+const totalDays =  bitcoinData.filter(el => el["price(USD)"] > 100).length
+console.log("total days over $100 -", totalDays);
+
+//7. And finally, let’s find the average bitcoin transaction fees between 2013 and 2015. Be sure to floor this value.
+
+const totalTransactionFees = bitcoinData.reduce ((total, el) => {
+  return total + el.fees;
+}, 0);
+
+const averageTransactionFees = Math.floor(totalTransactionFees / bitcoinData.length);
+
+console.log(averageTransactionFees);
+
+// Bonus 1
+function dayMapper(arr, property1, property2) {
+  const newArray = arr.map(function(el) {
+    const tempItem = {};
+    if(el[property1] !== undefined) {
+      tempItem[property1] = el[property1];
+    }
+    if(el[property2] !== undefined) {
+      tempItem[property2] = el[property2];
+    }
+    
+    return tempItem;
+  });
+    
+  return newArray;
+}
+
+const mappedArray = dayMapper(bitcoinData, "date", "price(USD)");
+
+console.log(mappedArray);
+
+// Bonus 2
+
+function averageValueOf(arr, property) {
+  const numValues = arr.filter(el => typeof el[property] === 'number');
+  const total = numValues.reduce((sum, el) => sum + el[property], 0);
+  return total / numValues.length;
+}
+
+const averagePrice = averageValueOf(bitcoinData, 'price(USD)');
+const averageFees = averageValueOf(bitcoinData, 'fees');
+console.log(averagePrice);
+console.log(averageFees);
+
+// **** BONUS ANSWERS PROVIDED BY INSTRUCTOR 
+
+// BONUS
+// 1.
+function dayMapper(arr, property1, property2) {
+  return arr.map((data) => {
+    const newObj = {};
+    if (data.hasOwnProperty(property1)) {
+      newObj[property1] = data[property1];
+    }
+    if (data.hasOwnProperty(property2)) {
+      newObj[property2] = data[property2];
+    }
+    return newObj;
+  });
+}
+
+// 2.
+function averageValueOfTxVolume(arr) {
+  const propValues = arr.map((data) => data["txVolume(USD)"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+function averageValueOfTxCount(arr) {
+  const propValues = arr.map((data) => data["txCount"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+function averageValueOfMarketcap(arr) {
+  const propValues = arr.map((data) => data["marketcap(USD)"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+function averageValueOfPrice(arr) {
+  const propValues = arr.map((data) => data["price(USD)"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+function averageValueOfExchangeVolume(arr) {
+  const propValues = arr.map((data) => data["exchangeVolume(USD)"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+function averageValueOfGeneratedCoins(arr) {
+  const propValues = arr.map((data) => data["generatedCoins"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+function averageValueOfFees(arr) {
+  const propValues = arr.map((data) => data["fees"]);
+  const total = propValues.reduce((acc, curr) => acc + curr, 0);
+  return total / propValues.length;
+}
+
+// 3.
+function generalAverageValue(arr, property) {
+  const propValues = arr.map((data) => data[property]);
+  const numValues = propValues.filter((value) => typeof value === "number");
+  if (numValues.length === 0) {
+    throw new Error(`Property ${property} does not have numeric values`);
+  }
+  const total = numValues.reduce((acc, curr) => acc + curr, 0);
+  const avgValue = total / numValues.length;
+  return [property, avgValue];
+}
+
+// 4.
+function priceRangeTally(arr) {
+  const prices = arr.map((data) => data["price(USD)"]);
+  const max = Math.max(...prices);
+  const min = Math.min(...prices);
+  const interval = 100;
+  const numRanges = Math.ceil((max - min) / interval);
+  const tally = Array(numRanges).fill(0);
+  for (let i = 0; i < prices.length; i++) {
+    const price = prices[i];
+    const rangeIndex = Math.floor((price - min) / interval);
+    tally[rangeIndex]++;
+  }
+  for (let i = 0; i < numRanges; i++) {
+    const rangeMin = min + i * interval;
+    const rangeMax = Math.min(rangeMin + interval, max);
+    const rangeLabel = `$${rangeMin.toFixed(2)} - $${rangeMax.toFixed(2)}`;
+    console.log(`${rangeLabel}: ${tally[i]} days`);
+  }
+}
+
+// 5.
+function max(arr) {
+  const numericalProperties = Object.keys(arr[0]).filter(
+    (key) => typeof arr[0][key] === "number",
+  );
+  const result = {};
+
+  for (const property of numericalProperties) {
+    const max = Math.max(...arr.map((item) => item[property]));
+    result[property] = max;
+  }
+
+  return result;
+}
+
+// 5.5
+function min(arr) {
+  const numericalProperties = Object.keys(arr[0]).filter(
+    (key) => typeof arr[0][key] === "number",
+  );
+  const result = {};
+
+  for (const property of numericalProperties) {
+    const min = Math.min(...arr.map((item) => item[property]));
+    result[property] = min;
+  }
+
+  return result;
+}
+
+// 6
+function ranges(maxObj, minObj) {
+  const result = {};
+
+  for (const key of Object.keys(maxObj)) {
+    const range = `${minObj[key]} - ${maxObj[key]}`;
+    result[key] = range;
+  }
+
+  return result;
+}
+
+
+*/
+
+
 // <<< ---------------------------------------------------------------------- CS Prep Cohort 122 (2024) POD  ---------------------------------------------------------------------- >>>
 /*
 POD Day 3 (Jun 12) - Regular - LargestAndSmallest -
@@ -779,7 +1115,7 @@ function largestAndSmallest(array) {
   // return [Math.min(...array), Math.max(...array)];
 }
 
-console.log(largestAndSmallest([1, 2, 3, 4, 5])); // should log [1, 5]
+// console.log(largestAndSmallest([1, 2, 3, 4, 5])); // should log [1, 5]
 // console.log(largestAndSmallest([90, 30, 44, 66, 10])); // should log [10, 90]
 // console.log(largestAndSmallest([16, -70, 122])); // should log [-70, 122]
 //                    or with a message (down below): 
