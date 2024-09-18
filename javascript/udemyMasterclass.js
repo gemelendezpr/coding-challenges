c698c17b2c43b8b0a2f0fbec58c86f90fb826471
gerarmc <gerardomelendezpro@gmail.com>
2024-09-18 12:22:20 -0400

    problem solving topic step1

--- javascript/udemyMasterclass.js
+++ javascript/udemyMasterclass.js
@@ -90,3 +90,29 @@ const testCases = [
 
 
 // Big O is O(n). Each loop is O(n) so there's two O(n). But the big picture is O(n). Because the number of operations is ( eventually) bounded by a multiple of n(say, 10n)
+// <<< ---------------------------------------------------------------------- Problem Solving ---------------------------------------------------------------------- >>>
+
+// Step 1: Understand the problem
+// Step 2: Explore Concrete Examples 
+// Step 3: Break it down 
+// Step 4: Solve/ Simplify 
+// Step 5: Look back and refactor
+
+/* ========== *
+* Exmample 1 *
+* =========== */
+
+// Write a function which takes two numbers and return their sum.
+
+// 1. Can I restate the problem in my own words?
+// "Implement addition"
+// 2. What are the inputs that go into the problem?
+    // - integers?
+    // - floats?
+    // - what about for larger numbers? 
+// 3. What are the outputs that should come from the solution to the problem?
+    // - int? float? string?
+// 4. Can the outputs be determine from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you set about solving the problem. Thats okay; its still worth considering the question early at this stage.)
+    // - What happen if we only receive one number? Do we add o or undefined etc
+// 5. How should I label the important pieces of data that are a part of the problem?
+    // - add -num1 - num2 -sum 
